c875267bf9ea1478a9e468e4a0c2e6346f3b2673
Alexei101 <alexakalexa649@gmail.com>
2024-09-03 20:07:28 +0300

    Translation of the recipe to rus

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from tkinter import *
 #from Tools.demo.spreadsheet import translate
 from googletrans import Translator
 
+
+
+
+
 if __name__ == '__main__':
 
     window = Tk()
@@ -45,12 +49,21 @@ if __name__ == '__main__':
 
     buttonTranslate = Button (
         frame,
-        text="Translate",
+        text="Translate(Google)",
         font=("Helvetica", 12),
         command=lambda: translate_recipe_to_rus()
     )
+
+    yc_translate = Button(
+        frame,
+        text="Translate(Yandex)",
+        font=("Helvetica", 12),
+        command=lambda: yc_translate()
+    )
+    yc_translate.pack(side="right", pady=20)
     buttonTranslate.pack(side = "right", padx = 20)
     button.pack(side="right", pady = 20)
+
     def print_recipe():
         view.delete(1.0, END)
         response = requests.get("https://www.themealdb.com/api/json/v1/1/random.php")
@@ -63,11 +76,56 @@ if __name__ == '__main__':
 
 
     def translate_recipe_to_rus():
-        v = view.get(1.0, END)
+        current_view = view.get(1.0, END)
         view.delete(1.0, END)
+        current_label = label.cget("text")
         translator = Translator()
-        translated_instructions = translator.translate(v, dest='ru').text
+        translated_instructions = translator.translate(current_view, dest='ru').text
+        translated_label = translator.translate(current_label, dest='ru').text
         view.insert(END, translated_instructions)
+        label.config(text=translated_label)
 
+    def yc_translate():
+        current_view = view.get(1.0, END)
+        view.delete(1.0, END)
+        current_label = label.cget("text")
+        IAM_TOKEN = 't1.9euelZqRz4qNms7JlMyeis6cx5XIje3rnpWalIrGyc6cmcuaj4_Lk4yclsrl8_dOOiNJ-e8LNQNK_d3z9w5pIEn57ws1A0r9zef1656VmpOJkM7OjZfGxo3Mzc-ZlceV7_zF656VmpOJkM7OjZfGxo3Mzc-ZlceV.Ink5ZHMkxurcoR-8Go73VQxUWUDkOtUXnU1bxfYdHZ4jjLlnJjK80p_Ni1Vow3qXHDlhxPRYE9F0s_YZ7ZklCA'
+        folder_id = 'b1gdr5mlfac2i8am95j9'
+        target_language = 'ru'
+        texts = current_view
+        texts_label = current_label
+
+        body = {
+            "targetLanguageCode": target_language,
+            "texts": texts,
+            "folderId": folder_id,
+        }
+        body_label = {
+            "targetLanguageCode": target_language,
+            "texts": texts_label,
+            "folderId": folder_id,
+        }
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer {0}".format(IAM_TOKEN)
+        }
+
+        response_view = requests.post('https://translate.api.cloud.yandex.net/translate/v2/translate',
+                                 json=body,
+                                 headers=headers
+                                 )
+        response_label = requests.post('https://translate.api.cloud.yandex.net/translate/v2/translate',
+                                      json=body_label,
+                                      headers=headers
+                                      )
+        yc_response_view = response_view.json()
+        yc_response_view_text = yc_response_view['translations'][0]['text']
+
+        yc_response_label = response_label.json()
+        yc_response_label_text = yc_response_label['translations'][0]['text']
+
+        view.insert(END, yc_response_view_text)
+        label.config(text=yc_response_label_text)
 
     Tk.mainloop(frame)
